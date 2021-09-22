d9d4880ef5b52d5cf8aa58325e027e16cd5174ed
Ashish Pradhan <ashishpradhan892@gmail.com>
2021-09-22 19:51:59 +0530

    3 ways to create observables done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,46 @@
-public class Main {
-    public static void main(String[] args) throws InterruptedException {
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.functions.Consumer;
 
-        Thread thread = new Thread(Main::runIt);
-        thread.start();
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args){
+        createObservablesWithJust();
+        createObservablesWithIterable();
+        createObservablesUsingCreate();
     }
-    public static void runIt() {
-        System.out.println("Thread name: "+Thread.currentThread().getName());
+
+    private static void createObservablesUsingCreate() {
+        Observable<Integer> observable = Observable.create(emitter -> {
+           emitter.onNext(1);
+           emitter.onNext(2);
+           emitter.onNext(3);
+           emitter.onComplete();
+        });
+        observable.subscribe(
+                integer -> System.out.println(integer),
+                error -> System.out.println(error.getLocalizedMessage()),
+                ()-> System.out.println("Completed!!"));
     }
+
+    private static void createObservablesWithIterable() {
+        List<Integer> list = List.of(1,2,3,4,5,6,7,8,9);
+        Observable<Integer> observable = Observable.fromIterable(list);
+        observable.subscribe(System.out::println);
+    }
+
+    public static void createObservablesWithJust() {
+        Observable<Integer> observable = Observable.just(1,2,3,4,5,6);
+
+        //Subscribe->Consumer
+        // We can reduce lines of code by using lambada and method reference
+        observable.subscribe(new Consumer<Integer>() {
+            @Override
+            public void accept(Integer integer) throws Throwable {
+                System.out.println(integer);
+            }
+        });
+
+    }
+
 }
