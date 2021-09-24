ad50dc4ec4cc7e5996d16dfc917745459da2afd6
Ashish Pradhan <ashishpradhan892@gmail.com>
2021-09-24 19:33:38 +0530

    Observable and Error vs Never done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,23 +1,65 @@
 import io.reactivex.rxjava3.core.Observable;
 import io.reactivex.rxjava3.functions.Consumer;
+import io.reactivex.rxjava3.internal.operators.observable.ObservableFromCallable;
 import io.reactivex.rxjava3.observables.ConnectableObservable;
 
 import java.util.List;
+import java.util.concurrent.Callable;
 
-
+import static java.lang.Thread.sleep;
 
 public class Main {
     public static void main(String[] args){
-       // Cold Observable create three ways:- just, create, fromIterable
-        // Hot Observable : from cold observable using connectable observable
-        createHotObservable();
+//        throwException();
+//        throwExceptionUsingCallable();
+//        createObservableUsingEmpty();
+        createObservableUsingNever();
     }
 
-    private static void createHotObservable() {
-       // Observable<Integer> observable = Observable.just(1,2,3,4,5);
-        ConnectableObservable<Integer> observable = Observable.just(1,2,3,4,5).publish(); //Hot Observable
-        observable.subscribe(item -> System.out.println("Observable-1: "+ item));
-        observable.subscribe(item -> System.out.println("Observable-2: "+ item));
-        observable.connect();
+    private static void throwException() {
+        //Create one Error Instance
+        Observable observable = Observable.error(
+                new Throwable("An Exception!!")
+        );
+        observable.subscribe(
+                System.out::println,
+                error -> System.out.println("Error Subscribe-1: " + error.hashCode())
+        );
+        observable.subscribe(
+                System.out::println,
+                error -> System.out.println("Error Subscribe-2: " + error.hashCode())
+        );
+    }
+
+    private static void throwExceptionUsingCallable() {
+        //Create different instance of error for different observer
+        Observable observable = Observable.error(() -> new Throwable("Error"));
+
+        observable.subscribe(
+                System.out::println,
+                error -> System.out.println("Error Subscribe-1: " + error.hashCode())
+        );
+        observable.subscribe(
+                System.out::println,
+                error -> System.out.println("Error Subscribe-2: " + error.hashCode())
+        );
+    }
+    private static void createObservableUsingEmpty() {
+        Observable observable = Observable.empty();
+        observable.subscribe(
+                System.out::println,
+                System.out::println,
+                ()->System.out.println("Completed")
+        );
+    }
+
+    private static void createObservableUsingNever() {
+        Observable observable = Observable.never();
+        observable.subscribe(
+                System.out::println,
+                System.out::println,
+                ()->System.out.println("Completed")
+        );
     }
 }
+
